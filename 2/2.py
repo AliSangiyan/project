86bb7f7b57009bced7a47a347ca62f4860daf36c
AliSangiyan <103169881+AliSangiyan@users.noreply.github.com>
2024-07-24 17:00:48 -0700

    Add files via upload

---
+++ 2/2.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=np.array([1,2,4])
+a=np.empty((3,4),dtype=np.int8)
+a
+
+p=([1,2,3],[4,5,6])
+p=np.empty_like(p,shape=(3,2))
+b=np.ones((2,4,3))
+b
+b=np.zeros((2,4,3))
+b
+b=np.full((2,4,3),8)
+b
+
